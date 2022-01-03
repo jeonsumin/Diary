744d8bd1f5a81db3a35780214e746f78999d559f
joensumin <soomis7@naver.com>
2022-01-03 11:02:59 +0900

    일기장 화면 과 즐겨찾기 화면 구성

---
+++ Diary/DirayCell.swift
@@ -0,0 +1,12 @@
+//
+//  DirayCell.swift
+//  Diary
+//
+//  Created by Terry on 2022/01/03.
+//
+
+import UIKit
+
+class DirayCell: UICollectionViewCell {
+    
+}
---
+++ Diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by Terry on 2022/01/03.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
